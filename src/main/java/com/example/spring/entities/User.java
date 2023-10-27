4ef4edb51dd272572210951e60c074718d040d79
Thinh Tran <thinhrom11@gmail.com>
2023-10-27 13:59:30 +0700

    chore: user implement user detail from spring

--- src/main/java/com/example/spring/entities/User.java
+++ src/main/java/com/example/spring/entities/User.java
@@ -6,13 +6,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.lang.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -23,20 +26,48 @@ import java.util.Set;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 @Table(name = "users")
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
     String username;
+
     @Email(message = "Email is not valid")
     @Column(unique = true)
-    @NonNull
+    @NotNull
     String email;
+
     @Column
     @Length(min = 8)
-    @NonNull
+    @NotNull
     String password;
-    @Past Instant dob;
+
+    @Past
+    Instant dob;
 
     @OneToMany(mappedBy = "user", orphanRemoval = true)
     @ToString.Exclude
     private Set<Post> posts = new LinkedHashSet<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Set.of(() -> "USER");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
